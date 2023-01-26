1b00515599f03d601d48cadeddc159e7b4570283
git-illite <71469298+git-illite@users.noreply.github.com>
2023-01-26 00:41:38 -0500

    Completed steps 34-37 in lab 3

---
+++ tests/unit/app.test.js
@@ -0,0 +1,10 @@
+// tests/unit/app.test.js
+
+const request = require('supertest');
+//const logger = require('../../src/logger');
+
+const app = require('../../src/app');
+
+describe("requests for resources that can't be found can't be found", () => {
+  test('should return a 404 response', () => request(app).get('/notrealpage').expect(404));
+});
